dea499636761b3b1a03bb697893dde5ad2b67cb8
hainenber <dotronghai96@gmail.com>
2024-10-22 23:10:07 +0700

    feat(script): check for Java installation
    Signed-off-by: hainenber <dotronghai96@gmail.com>

--- start-jenkins.sh
+++ start-jenkins.sh
@@ -13,9 +13,28 @@ error_and_exit() {
     exit "${@:-1}" 
 }
 
-# TODO: Check if Java 8+ installation is available.
+# Check if Java installation is available.
+# Else, check if Java installation is Java17 or else.
+# Beginning with the Jenkins 2.463 weekly release (scheduled for release on June 18, 2024), Jenkins requires Java 17 or newer
+# Source: https://www.jenkins.io/blog/2024/06/11/require-java-17/
+if [ -z "${JAVA_HOME}" ] || ! command -v java 2>&1 >/dev/null ; then 
+    error_and_exit "Current machine does not have Java installation to proceed further"  1
+else
+    # Get the Java version
+    java_version=$(java -version 2>&1 | head -n 1 | awk -F '"' '{print $2}')
+    # Extract major version
+    # Handles the version format. Java 17 and earlier are in the form 1.x, while Java 9+ drops the leading 1.
+    java_major_version=$(echo "${java_version}" | awk -F '.' '{if ($1 == "1") print $2; else print $1}')
+    # Check if the major version is 17 or higher
+    if [[ "$java_major_version" -le 17 ]]; then
+        error_and_exit "Current machine has Java version that is less than 17 (version: $java_version)" 1
+    fi
+fi
 
-# TODO: Fetch latest Jenkins version from remote registry.  
+# Get Jenkins version pinned in "jenkins.version"
+if [ ! -f "./jenkins.version" ]; then
+    error_and_exit "Root directory does not have \"jenkins.version\" to select approriate Jenkins version" 1
+fi
 
 # Install Jenkins if the WAR file does not exist
 # Can execute the download task either via "curl" or "wget".
