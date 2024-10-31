997742bc98f5901c593a9b54e692762e29a71455
hainenber <dotronghai96@gmail.com>
2024-10-31 23:07:20 +0700

    chore(ci): fix shellcheck lint issue
    Signed-off-by: hainenber <dotronghai96@gmail.com>

--- jenkins-agent/setup-multipass-vm-as-jenkins-agent.sh
+++ jenkins-agent/setup-multipass-vm-as-jenkins-agent.sh
@@ -3,5 +3,5 @@
 # Build Ubuntu VMs that are readily Jenkins agent.
 
 for i in seq 2 2; do
-    multipass launch --cpus 1 --disk 10G --memory 2G noble --name java-app-builder-2 --cloud-init cloud-config.yaml
+    multipass launch --cpus 1 --disk 10G --memory 2G noble --name "java-app-builder-${i}" --cloud-init cloud-config.yaml
 done
