d3e0988cb922eba8e2546356e939c1149a5c32dd
hainenber <dotronghai96@gmail.com>
2024-10-31 22:34:37 +0700

    fix(jenkins/init-hook): workaround to pre-approve pending scripts that are blocking seed job
    Signed-off-by: hainenber <dotronghai96@gmail.com>

--- hook-scripts/init/00-trigger-seed-job.groovy
+++ hook-scripts/init/00-trigger-seed-job.groovy
@@ -2,6 +2,9 @@ import jenkins.model.Jenkins
 import hudson.model.Cause
 import hudson.model.Result
 
+import org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval
+import org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval.PendingScript
+
 // Trigger seed job.
 def jenkins = Jenkins.get()
 def job = jenkins.getItemByFullName('seed')
@@ -9,6 +12,18 @@ def job = jenkins.getItemByFullName('seed')
 if (job == null) {
     println("[INIT] Seed job not found. Exiting script")
     return
+}
+
+// List out scripts for approval
+// Only approve hash of jobs declared in ./data/jobs
+// This is to allow seed job to run normally. 
+ScriptApproval scriptApproval = ScriptApproval.get()
+for (PendingScript pendingScript in scriptApproval.getPendingScripts()) {
+    String scriptHash = pendingScript.getHash()
+    // build_jenkins_plugin_manager_jar.groovy
+    if (scriptHash == 'SHA512:fea3f17ae00259d0e9fbe1cc6fc65e16a507a11ed2970744f627b9099ccde2ca7c8aca772d4ebec2961e9f637a03bd50957cbeaa0b746f96a97aa64f508b7bae') {
+        scriptApproval.approveScript(scriptHash)
+    }
 }
 
 // Temporarily allow controller node to execute seed job.
