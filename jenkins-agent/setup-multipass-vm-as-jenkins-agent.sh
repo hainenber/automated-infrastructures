84641024748d3d4b51e87e44601d082460688eb8
hainenber <dotronghai96@gmail.com>
2024-11-02 22:29:08 +0700

    fix(jenkins/agent): only setup Multipass VMs if not exist
    Signed-off-by: hainenber <dotronghai96@gmail.com>

--- jenkins-agent/setup-multipass-vm-as-jenkins-agent.sh
+++ jenkins-agent/setup-multipass-vm-as-jenkins-agent.sh
@@ -2,6 +2,13 @@
 
 # Build Ubuntu VMs that are readily Jenkins agent.
 
-for i in seq 2 2; do
-    multipass launch --cpus 1 --disk 10G --memory 2G noble --name "java-app-builder-${i}" --cloud-init cloud-config.yaml
+current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+for i in $(seq 1 2); do
+    vm_name="java-app-builder-${i}"
+    if ! multipass list --format=csv | grep "${vm_name}" >/dev/null 2>&1 ; then
+        multipass launch --cpus 1 --disk 10G --memory 2G noble --name "${vm_name}" --cloud-init "${current_dir}/cloud-config.yaml"
+    else
+        echo "[INFO] ${vm_name} exists."
+    fi
 done
