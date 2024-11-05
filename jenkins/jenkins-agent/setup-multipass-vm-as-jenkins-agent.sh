aaf9d023ba156af92a7e895eba41867355cc65e4
hainenber <dotronghai96@gmail.com>
2024-11-05 23:24:31 +0700

    chore(jenkins/agent): add INFO logs about agent's VM creation
    Signed-off-by: hainenber <dotronghai96@gmail.com>

--- jenkins/jenkins-agent/setup-multipass-vm-as-jenkins-agent.sh
+++ jenkins/jenkins-agent/setup-multipass-vm-as-jenkins-agent.sh
@@ -7,8 +7,10 @@ current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 for i in $(seq 1 2); do
     vm_name="java-app-builder-${i}"
     if ! multipass list --format=csv | grep "${vm_name}" >/dev/null 2>&1 ; then
+        echo "[INFO] VM ${vm_name} does not exist. Creating..."
         multipass launch --cpus 1 --disk 10G --memory 2G noble --name "${vm_name}" --cloud-init "${current_dir}/cloud-config.yaml"
+        echo "[INFO] Done creating VM ${vm_name}"
     else
-        echo "[INFO] ${vm_name} exists."
+        echo "[INFO] VM ${vm_name} exists."
     fi
 done
