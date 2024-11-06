80c1b15b0ec0e6e3ecfff1ca2f240e4045ead5d4
hainenber <dotronghai96@gmail.com>
2024-11-06 23:17:41 +0700

    feat(jenkins/agent): setup Jenkins agent to run JS scripts
    Signed-off-by: hainenber <dotronghai96@gmail.com>

--- jenkins/jenkins-agent/setup-multipass-vm-as-jenkins-agent.sh
+++ jenkins/jenkins-agent/setup-multipass-vm-as-jenkins-agent.sh
@@ -4,11 +4,24 @@
 
 current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-for i in $(seq 1 2); do
+# Java
+for i in $(seq 1 1); do
     vm_name="java-app-builder-${i}"
     if ! multipass list --format=csv | grep "${vm_name}" >/dev/null 2>&1 ; then
         echo "[INFO] VM ${vm_name} does not exist. Creating..."
-        multipass launch --cpus 1 --disk 10G --memory 2G noble --name "${vm_name}" --cloud-init "${current_dir}/cloud-config.yaml"
+        multipass launch --cpus 1 --disk 10G --memory 2G noble --name "${vm_name}" --cloud-init "${current_dir}/cloud-config.java.yaml"
+        echo "[INFO] Done creating VM ${vm_name}"
+    else
+        echo "[INFO] VM ${vm_name} exists."
+    fi
+done
+
+# JavaScript, TypeScript
+for i in $(seq 1 1); do
+    vm_name="javascript-app-builder-${i}"
+    if ! multipass list --format=csv | grep "${vm_name}" >/dev/null 2>&1 ; then
+        echo "[INFO] VM ${vm_name} does not exist. Creating..."
+        multipass launch --cpus 1 --disk 10G --memory 2G noble --name "${vm_name}" --cloud-init "${current_dir}/cloud-config.javascript.yaml"
         echo "[INFO] Done creating VM ${vm_name}"
     else
         echo "[INFO] VM ${vm_name} exists."
