3ac2223699bc402f2bf01a3ffe29d4a610d3a265
hainenber <dotronghai96@gmail.com>
2024-11-12 21:28:23 +0700

    chore(*): prettify whole codebase
    Signed-off-by: hainenber <dotronghai96@gmail.com>

--- README.md
+++ README.md
@@ -8,20 +8,27 @@ ACTIVE
 
 ## Prerequisites
 
-* Java17+ installation to run Jenkins.
-* [`multipass`](https://multipass.run/) for creating Jenkins agents.
-* [`jq`](https://jqlang.github.io/jq/).
+- Java17+ installation to run Jenkins.
+- [`multipass`](https://multipass.run/) for creating Jenkins agents.
+- [`jq`](https://jqlang.github.io/jq/).
 
 ## Usage
+
+### Jenkins
 
 Run `bash ./jenkins/start-jenkins.sh` for the very first time!
 
 If you add new Jenkins job definition in `./jobs`, Jenkins will fail to run its seed job as the newly Jenkins job definition
 is not yet approved. You'll need to login to Jenkins, manually approve the job definition and rerun the startup script.
 
+### Sonatype Nexus
+
+Run `bash ./sonatype/start-nexus.sh` to setup repository/artifact management Sonatype Nexus
 
 ## SBOMs
+
 - [Java](https://www.oracle.com/java/)
 - [Multipass](https://multipass.run/)
 - [`jq`](https://jqlang.github.io/jq/)
 - [UpdateCLI](https://github.com/updatecli/updatecli?tab=readme-ov-file)
+- [sdkman](https://sdkman.io/)
--- scripts/disable_github_actions.js
+++ scripts/disable_github_actions.js
@@ -1,41 +1,51 @@
-import { Octokit } from 'octokit';
+import { Octokit } from "octokit";
 
 (async () => {
-    // Setup Octokit (GH) client
-    const octokit = new Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
-    const commonHeaders = {  'X-GitHub-Api-Version': '2022-11-28' };
+  // Setup Octokit (GH) client
+  const octokit = new Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
+  const commonHeaders = { "X-GitHub-Api-Version": "2022-11-28" };
 
-    // Login
-    const { data: { login }} = await octokit.rest.users.getAuthenticated();
+  // Login
+  const {
+    data: { login },
+  } = await octokit.rest.users.getAuthenticated();
 
-    // Get all personal repos
-    const forkedRepoData = (await octokit.paginate('GET /users/{username}/repos', {
-        username: login,
+  // Get all personal repos
+  const forkedRepoData = (
+    await octokit.paginate("GET /users/{username}/repos", {
+      username: login,
+      headers: commonHeaders,
+    })
+  )
+    .filter((repoData) => repoData["fork"] === true)
+    .map((repoData) => ({
+      id: repoData["id"],
+      name: String(repoData["name"]),
+      full_name: String(repoData["full_name"]),
+      default_fork_branch: String(repoData["default_branch"]),
+    }));
+
+  // Disable GH action for forked repos
+  for (const repoData of forkedRepoData) {
+    try {
+      console.log(
+        `[INFO] Disabling GitHub Actions runner for ${repoData.full_name}...`,
+      );
+      await octokit.request("PUT /repos/{owner}/{repo}/actions/permissions", {
+        owner: login,
+        repo: repoData.name,
+        enabled: false,
         headers: commonHeaders,
-    }))
-    .filter(repoData => repoData['fork'] === true)
-    .map(repoData => ({
-        id: repoData['id'],
-        name: String(repoData['name']),
-        full_name: String(repoData['full_name']),
-        default_fork_branch: String(repoData['default_branch']),
-    }))
-
-    // Disable GH action for forked repos 
-    for (const repoData of forkedRepoData) {
-        try {
-            console.log(`[INFO] Disabling GitHub Actions runner for ${repoData.full_name}...`)
-            await octokit.request('PUT /repos/{owner}/{repo}/actions/permissions', {
-                owner: login,
-                repo: repoData.name,
-                enabled: false,
-                headers: commonHeaders,
-            });
-            console.log(`[INFO] ✅ Disable GitHub Actions runner for ${repoData.full_name}`)
-            console.log()
-        } catch (e) {
-            console.log(`[ERROR] ❌ Error disabling GitHub Actions runner for ${repoData.full_name}: ${e}`)
-            console.log()
-        }
+      });
+      console.log(
+        `[INFO] ✅ Disable GitHub Actions runner for ${repoData.full_name}`,
+      );
+      console.log();
+    } catch (e) {
+      console.log(
+        `[ERROR] ❌ Error disabling GitHub Actions runner for ${repoData.full_name}: ${e}`,
+      );
+      console.log();
     }
+  }
 })();
