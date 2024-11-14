289611df310e333fcde831231da5fed57d53a16d
hainenber <dotronghai96@gmail.com>
2024-11-14 23:28:04 +0700

    chore(nexus/init-script): exit script with non-zero code after logging fatal message
    Signed-off-by: hainenber <dotronghai96@gmail.com>

--- scripts/start-nexus.js
+++ scripts/start-nexus.js
@@ -5,9 +5,10 @@ import { configure, getConsoleSink, getLogger } from "@logtape/logtape";
 import { head } from "es-toolkit";
 import { homedir } from "os";
 import { cwd } from "process";
-import { generateLogFilenameWithTimestamp } from "./utils.js";
+import { generateLogFilenameWithTimestamp, PROJECT_NAME } from "./utils.js";
 
 // Constants
+const SERVICE = "nexus";
 const VERSION_LIMIT = 3;
 const NEXUS_COMPATIBLE_JAVA_MAJOR_VERSION = "17";
 
@@ -15,20 +16,21 @@ const NEXUS_COMPATIBLE_JAVA_MAJOR_VERSION = "17";
   // Configure logger
   await configure({
     sinks: { console: getConsoleSink() },
-    loggers: [{ category: "auto-infra", level: "debug", sinks: ["console"] }],
+    loggers: [{ category: PROJECT_NAME, level: "debug", sinks: ["console"] }],
   });
-  const logger = getLogger(["auto-infra"]);
+  const logger = getLogger([PROJECT_NAME, SERVICE]);
 
   // Start Sonatype Nexus
   // Find Nexus directory
   const sonatypeFolderPath = pathJoin(cwd(), "sonatype");
   const sonatypeBinaryPaths = readdirSync(sonatypeFolderPath)
     .map((fileName) => pathJoin(sonatypeFolderPath, fileName))
-    .filter((fileName) => lstatSync(fileName).isDirectory() && fileName.includes("nexus-"));
+    .filter((fileName) => lstatSync(fileName).isDirectory() && fileName.includes(`${SERVICE}-`));
 
   // Validating presence of Nexus binary path(s)
   if (sonatypeBinaryPaths.length == 0) {
     logger.fatal("Not found any Nexus directory");
+    process.exit(1);
   } else if (sonatypeBinaryPaths.length > 3) {
     logger.warn(
       `Found more than 1 Nexus directory ${sonatypeBinaryPaths}. Please keep them to minimal ${VERSION_LIMIT}`,
@@ -42,6 +44,7 @@ const NEXUS_COMPATIBLE_JAVA_MAJOR_VERSION = "17";
   const jvmCandidatesPath = pathJoin(homedir(), ".sdkman", "candidates", "java");
   if (!lstatSync(jvmCandidatesPath)) {
     logger.fatal("Not found the path used by sdkman to install Java.");
+    process.exit(1);
   }
   const jvmCandidates = readdirSync(jvmCandidatesPath)
     .map((fileName) => pathJoin(jvmCandidatesPath, fileName))
@@ -53,6 +56,7 @@ const NEXUS_COMPATIBLE_JAVA_MAJOR_VERSION = "17";
   // Validating presence of suitable JVM
   if (jvmCandidates.length == 0) {
     logger.fatal(`Not found any Java ${NEXUS_COMPATIBLE_JAVA_MAJOR_VERSION} candidate`);
+    process.exit(1);
   }
 
   // Creating directory for Nexus logs
@@ -61,7 +65,7 @@ const NEXUS_COMPATIBLE_JAVA_MAJOR_VERSION = "17";
 
   // Start Nexus
   const jvmCandidate = head(jvmCandidates);
-  const nexusLogFilename = generateLogFilenameWithTimestamp("nexus");
+  const nexusLogFilename = generateLogFilenameWithTimestamp(SERVICE);
   await execa({
     cwd: sonatypeBinaryPath,
     env: { INSTALL4J_HOME: jvmCandidate },
